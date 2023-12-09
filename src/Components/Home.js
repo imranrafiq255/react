f01cd4daa5a359d998f73529dcf7a38738f43f93
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-09 15:51:11 +0500

    the change is

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import axios from "axios";
 const Home = () => {
   useEffect(() => {
-    axios.get("/api/v1").then((response) => console.log(response.data.success));
-    console.log("here");
+    axios
+      .get("https://imran-react.vercel.app/api/v1")
+      .then((response) => console.log(response.data.success))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
   return <>hOME</>;
 };
 
