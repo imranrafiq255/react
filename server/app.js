2ad3f9f027a6d97ad4768f8f570577f9c2933a1a
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-09 15:06:07 +0500

    this is server change

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+  });
+});
 const port = 4500;
 app.listen(port, () => {
   console.log("Server is running on: " + port);
